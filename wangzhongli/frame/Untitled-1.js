134cee75a734efd11a601dfb91af2f7507f2431a
smallg0at <10290487+smallg0at@user.noreply.gitee.com>
2022-01-15 22:40:00 +0800

    Add 4 person

---
+++ wangzhongli/frame/Untitled-1.js
@@ -0,0 +1,15 @@
+var arr =["思政课 期末考试", "论文*2","论文*1","荣誉学院论坛 论文 小组","不定积分作业 单号"];
+for(var i = 0 ; i < arr.length; i++){
+ console.log(i,"项目：" ,arr[i]);
+}
+
+var arr = ['不定积分作业 单号','荣誉学院论坛 论文 小组', '论文*2', '思政课 期末考试'];	
+	arr.forEach(function(value,){
+		
+		console.log(value);
+		})
+
+
+
+
+
